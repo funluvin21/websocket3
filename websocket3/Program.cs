b19017cf1e2773ec54db239907824f9e0e817067
Yoojin oh <funluvin@naver.com>
2020-11-04 22:28:16 +0900

    20201103

--- websocket3/Program.cs
+++ websocket3/Program.cs
@@ -8,11 +8,6 @@ namespace websocket3
         {
             Console.WriteLine("웹 소켓 서버를 시작합니다.");
 
-            wsServer ws = new wsServer("127.0.0.1", 8181);
-
-            //엔터를 입력하면 앱이 종료됩니다.
-            Console.ReadLine();
-
         }
     }
 }
